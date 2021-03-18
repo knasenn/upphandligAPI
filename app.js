f6d8f3b52c97bc49ec696f7feeaf0c9f28c64e3c
Kna Senn <knasenn@gmail.com>
2021-03-18 19:44:45 +0100

    created a control folder

--- app.js
+++ app.js
@@ -1,29 +1,57 @@
-const express = require("express");
+// https://github.com/knasenn/upphandlingAPI
+// https://github.com/wiur83/exjobb-komo
+
+// Fixa så att JsonWebToken funkar (backend?)
+// const verify = require("../models/verifyToken");
+// https://github.com/wiur83/exjobb-komo/blob/master/controllers/voice.js
+
+// **** Fixa så .env har token i sig
+// Lägg till gitignore (bla .env)
+// Kör MVC modellen
+// och kör module.export
+
+//Importing modules
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv");
+
+//Variables
+const port = 8333;
 const app = express();
+const verify = require("./models/verifyToken");
 
+//Importing controllers
+const authRoute = require("./controllers/auth");
 
-//VARIABLES
-const port = 3000;
+//Config
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-//MIDDLEWARE
-app.use("/middleware", (req,res,next) => {
-  console.log("middleware test successful");
-  next();
+//DB connect
+let db = new sqlite3.Database('./db/texts.sqlite', (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+      console.log("connected to db");
+  }
 });
 
-//ROUTES
-app.get("/middleware", (req,res) => {
-  res.send("we are on middleware");
-});
 
-app.get("/", (req,res) => {
-  res.send("we are on home");
+//Index GET
+app.get("/", (req, res) => {
+    res.send("hello /");
 });
 
 
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Welcome to the API'
+  });
+});
 
-
-
+//Middleware
+app.use("/user", authRoute);
 
 app.listen(port, () => console.log(`the server is running on port ${port}`));
