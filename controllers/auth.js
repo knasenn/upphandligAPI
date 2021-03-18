66fa7ba11ce6b15b8725a6a17b34fdd460d57266
Kna Senn <knasenn@gmail.com>
2021-03-18 20:50:39 +0100

    updated auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = require('express').Router();
 const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const sqlite3 = require('sqlite3').verbose();
 // const fetch = require("node-fetch");
 
-// const { registerValidation, loginValidation } = require("../validation");
+const { registerValidation, loginValidation } = require("../models/validation");
 const url_adress = "http://localhost:8333";
 
 
@@ -19,18 +19,14 @@ let db = new sqlite3.Database('./db/texts.sqlite', (err) => {
 //Static files
 router.use("/", express.static("assets"));
 
-//Index GET
+//rego GET
 router.get("/rego", (req, res) => {
     res.send("rego /");
 });
 
-//
-//
-// //register GET
-// router.get("/register1", (req, res) => {
-//     //Renders the register view
-//     res.render("../views/register.ejs");
-// });
+
+
+
 // //register POST
 // router.post("/register", async (req, res) => {
 //     try {
@@ -62,42 +58,42 @@ router.get("/rego", (req, res) => {
 //           res.status(400).send(err);
 //     }
 // });
-//
-// //register-create POST
-// router.post("/register-push", async (req, res) => {
-//     res.json({ msg: "success" });
-//     // //Validation
-//     // const { error } = registerValidation(req.body);
-//     // if (error) return res.json({ msg: "error", text: error.details[0].message });
-//     //
-//     // //Hash password
-//     // const salt = await bcrypt.genSalt(10);
-//     // const hashedPassword = await bcrypt.hash(req.body.password, salt);
-//     //
-//     // //Random ID generated
-//     // let random_id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-//     //
-//     //
-//     // //Check if email exist
-//     // db.each("SELECT COUNT(*) AS total FROM users WHERE email LIKE ?",
-//     // req.body.email,(err, row) => {
-//     //     if (row.total == 1) {
-//     //         //Email exists
-//     //     	return res.json({ msg: "error", text: "Email already exists" });
-//     //     } else {
-//     //         //Email does not exists
-//     //         db.run("INSERT INTO users (name, email, password, backup_id) VALUES (?, ?, ?, ?)",
-// 		// 		req.body.name, req.body.email, hashedPassword, random_id, (err) => {
-// 		// 		if (err) {
-// 		// 			res.json({ msg: "error", text: "Something went wrong while writing to database." });
-// 		// 		} else {
-//     //                 res.json({ msg: "success" });
-//     //             }
-// 		//     });
-//     //     }
-//     // });
-// });
-//
+
+//register-create POST
+router.post("/register-push", async (req, res) => {
+
+    //Validation
+    const { error } = registerValidation(req.body);
+    if (error) return res.json({ msg: "error", text: error.details[0].message });
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Random ID generated
+    let random_id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+
+    //Check if email exist
+    db.each("SELECT COUNT(*) AS total FROM users WHERE email LIKE ?",
+    req.body.email,(err, row) => {
+        if (row.total == 1) {
+            //Email exists
+        	return res.json({ msg: "error", text: "Email already exists" });
+        } else {
+            //Email does not exists
+            db.run("INSERT INTO users (name, email, password, backup_id) VALUES (?, ?, ?, ?)",
+				req.body.name, req.body.email, hashedPassword, random_id, (err) => {
+				if (err) {
+					res.json({ msg: "error", text: "Something went wrong while writing to database." });
+				} else {
+                    res.json({ msg: "success" });
+                }
+		    });
+        }
+    });
+});
+
 // //login GET
 // router.get("/login", (req, res) => {
 //     //Renders the login view
@@ -153,42 +149,42 @@ router.get("/rego", (req, res) => {
 //           res.render("error.ejs", {msg: "Something went wrong"});
 //     }
 // });
-//
-//
-//
-//
-//
-//
-// //login-push POST
-// router.post("/login-push", async (req, res) => {
-//     //Validation email and password
-//     const { error } = loginValidation(req.body);
-//     if (error) return res.json({ msg: "error", text: error.details[0].message });
-//
-//     //Check if email exist
-//     db.each("SELECT COUNT(*) AS total FROM users WHERE email LIKE ?",
-//     req.body.email, async (err, row) => {
-//         if (row.total == 0) {
-//             //Email not exists
-//             res.json({ msg: "error", text: "Email not found" });
-//         } else {
-//             //email exist
-//             db.each("SELECT * FROM users WHERE email LIKE ?",
-//             req.body.email, async (err, row) => {
-//                 if (err) {
-//                     //Err
-//                 	return res.json({ msg: "error", text: "Something went wrong" });
-//                 } else {
-//                     //Check if password is correct
-//                     const validPass = await bcrypt.compare(req.body.password, row.password);
-//                     if (!validPass) return res.json({ msg: "error", text: "Wrong password!!" });
-//                     const token = jwt.sign({id: row.backup_id}, process.env.TOKEN_SECRET);
-//                     res.json({ msg: "token", text: token, backup_id: row.backup_id });
-//                 }
-//             });
-//         }
-//     });
-// });
+
+
+
+
+
+
+//login-push POST
+router.post("/login-push", async (req, res) => {
+    //Validation email and password
+    const { error } = loginValidation(req.body);
+    if (error) return res.json({ msg: "error", text: error.details[0].message });
+
+    //Check if email exist
+    db.each("SELECT COUNT(*) AS total FROM users WHERE email LIKE ?",
+    req.body.email, async (err, row) => {
+        if (row.total == 0) {
+            //Email not exists
+            res.json({ msg: "error", text: "Email not found" });
+        } else {
+            //email exist
+            db.each("SELECT * FROM users WHERE email LIKE ?",
+            req.body.email, async (err, row) => {
+                if (err) {
+                    //Err
+                	return res.json({ msg: "error", text: "Something went wrong" });
+                } else {
+                    //Check if password is correct
+                    const validPass = await bcrypt.compare(req.body.password, row.password);
+                    if (!validPass) return res.json({ msg: "error", text: "Wrong password!!" });
+                    const token = jwt.sign({id: row.backup_id}, process.env.TOKEN_SECRET);
+                    res.json({ msg: "token", text: token, backup_id: row.backup_id });
+                }
+            });
+        }
+    });
+});
 
 
 module.exports = router;
